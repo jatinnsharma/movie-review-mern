d4b05cb39b3cd0112559a94c6548d00e6ef4c7d0
jatin <jatinsharma2231@gmail.com>
2023-11-10 23:47:56 +0530

    Create user model

--- server/db/index.js
+++ server/db/index.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/review_app',{
-    useNewUrlParser: true, 
-    useUnifiedTopology: true,
     family: 4,
 }).then(()=>{
     console.log('db is connected!')
