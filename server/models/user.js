73dd93e9627c3ffe458d36ec1fe8b60c68016f57
jatin <jatinsharma2231@gmail.com>
2023-11-11 00:25:44 +0530

    hashing password using bcrypt and save inside mongodb

--- server/models/user.js
+++ server/models/user.js
@@ -1,5 +1,5 @@
  const mongoose = require('mongoose')
-
+const bcrypt = require('bcrypt')
  const userSchema = mongoose.Schema({
     name:{
         type:String,
@@ -10,12 +10,23 @@
         type:String,
         trim:true,
         required:true,
-        unique:true
+        unique:true,
     },
     password:{
         type:String,
         required:true,
     }
  })
+
+// whenever we are saving this file before we save this file
+// we want to run this function.
+// hash this password before we saved this record    
+// hashing password is async task 
+userSchema.pre('save',async function(next){
+if(this.isModified('password')){
+    this.password = await bcrypt.hash(this.password,10)
+}
+next();
+})
 
 module.exports = mongoose.model("User",userSchema)
