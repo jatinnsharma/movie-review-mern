52dfcea8fe8756d56b90acf084b1eaa4080bef9c
jatin <jatinsharma2231@gmail.com>
2023-11-11 23:29:45 +0530

    Sending verification email

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,6 +1,9 @@
 // Request will have the information,whatever that is,comming form our frontend.
 // Response : whatever you want to send to your frontend will handle by response. 
+const nodemailer = require('nodemailer')
 const User = require('../models/user')
+const EmailVerificationToken = require('../models/emailVerificationToken')
+
 
 
 exports.create = async (req,res)=>{
@@ -17,18 +20,44 @@ exports.create = async (req,res)=>{
      
     // save inside our database which async task
     await newUser.save()
+
+    // generate 6 digit otp 
+    let OTP = " ";
+    for(let i = 0;i<=5;i++){
+        const randomVal = Math.round(Math.random()*9)
+        OPT+=randomVal;
+    }
+    // store otp inside our db 
+    const newEmailVaificationToken = new EmailVerificationToken({owner:newUser._id,
+        token:OTP,
+    })
+
+    await newEmailVaificationToken.save();
+
+    // send that otp to our user 
     
-    // var transport = nodemailer.createTransport({
-    //     host: "sandbox.smtp.mailtrap.io",
-    //     port: 2525,
-    //     auth: {
-    //       user: "7ac9c9a456c06e",
-    //       pass: "6886953acb65d1"
-    //     }
-    //   });
-    
+    var transport = nodemailer.createTransport({
+        host: "sandbox.smtp.mailtrap.io",
+        port: 2525,
+        auth: {
+          user: "7ac9c9a456c06e",
+          pass: "6886953acb65d1"
+        }
+      });
+
+
+      transport.sendMail({
+        from:"verfication@moviereviewapp.com",
+        to:newUser.email,
+        subject:'Email Verification',
+        html:`
+        <p>Your verification OTP Number</p>
+        <h1>${OTP}</h1>
+        `
+      })
+
     // 201 status code used for creating
-    res.status(201).json({user:newUser})
+    res.status(201).json({message:'Please verify your email. OTP has been send to your email account'})
    
 }
 
