5e71b3353c9e0162f139d0ad58d716f6c15e46c1
jatin <jatinsharma2231@gmail.com>
2023-11-13 18:28:08 +0530

    model-for-password-reset-token

---
+++ server/models/passwordResetToken.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const passwordResetTokenSchema = mongoose.Schema({
+    owner:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true,
+    },
+    token:{
+        type:String,
+        required:true,
+    },
+    createAt:{
+        type:Date,
+        expires:3600,
+        default:Date.now()
+    },
+    isVerified:{
+        type:Boolean,
+        required:true,
+        default:false,
+    }
+})
+
+// arrow function don't have this keyword that we are using normal function
+passwordResetTokenSchema.pre('save',async function(next){
+    if(this.isModified('token')){
+        this.token = await bcrypt.hash(this.token,10)
+    }
+    next();
+    })
+
+    passwordResetTokenSchema.methods.compaireToken = async function (token) {
+      const result =   await bcrypt.compare(token , this.token)
+      return result;
+    }
+
+module.exports = mongoose.model(
+    "PasswordResetToken",
+    passwordResetTokenSchema
+)
+
